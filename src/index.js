9fc12ca9e3c3a3f9cacfb87c0f3c2973d58d2d7c
“bzhang420” <bzhang420@gatech.edu>
2023-01-26 22:01:05 -0500

    fixed project structure

--- src/index.js
+++ src/index.js
@@ -4,22 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./components/login_component";
-import Dashboard from "../dashboard";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="login" element={<Login />} />
-        <Route path="dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
