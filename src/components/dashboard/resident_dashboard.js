191b81e08d1b89539fa72b37d9c3a00909ada742
Wei <wjeng7@gatech.edu>
2023-02-20 13:11:28 -0500

    change ui and logic for signup page

--- src/components/dashboard/resident_dashboard.js
+++ src/components/dashboard/resident_dashboard.js
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
 import "./resident_dashboard.css";
-import Bar from "../../images/bar.png";
+
+// dummy graph image files to be removed
+import BarImage from "../../images/bar.png";
 import Pie from "../../images/pie.svg";
 import Scatter from "../../images/scatter.jpeg";
 
+// Rechart UI
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+// ANTD UI
 import { Col, Row } from "antd";
 import { Layout } from "antd";
 import { Button } from "antd";
 import { DatePicker, message } from "antd";
 
-const { Header, Content } = Layout;
+const { RangePicker } = DatePicker;
 
+// ANTD CSS - TO be migrated to a seperate CSS file
+const { Header, Content } = Layout;
 const headerStyle = {
   textAlign: "center",
   color: "#fff",
@@ -27,12 +44,58 @@ const contentStyle = {
   backgroundColor: "#108ee9",
 };
 
-const Resident_dashboard = () => {
-  const [image, setImage] = useState("");
-  const [temp, setTfvalue] = useState("");
+// rechart dummy data to be removed
 
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+const Resident_dashboard = () => {
+  const [image, setImage] = useState("BarImage");
   const [date, setDate] = useState(null);
-  const handleChange = (value) => {
+
+  const handleDateChange = (value) => {
     message.info(
       `Selected Date: ${value ? value.format("YYYY-MM-DD") : "None"}`
     );
@@ -41,8 +104,9 @@ const Resident_dashboard = () => {
 
   const displayOnChange = (event) => {
     const valueSelectedByUser = parseInt(event.target.value);
+
     if (valueSelectedByUser === 1) {
-      setImage("Bar");
+      setImage("BarImage");
     }
 
     if (valueSelectedByUser === 2) {
@@ -52,13 +116,7 @@ const Resident_dashboard = () => {
     if (valueSelectedByUser === 3) {
       setImage("Scatter");
     }
-    setTfvalue("false");
   };
-
-  function handleClick(event) {
-    // event.preventDefault();
-    setTfvalue("true");
-  }
 
   return (
     <>
@@ -66,8 +124,8 @@ const Resident_dashboard = () => {
         <Header style={headerStyle}>Header Bar</Header>
         <Content style={contentStyle}>
           <Row>
-            <Col flex={4}>
-              <DatePicker onChange={handleChange} />
+            <Col flex={3}>
+              <RangePicker onChange={handleDateChange} />
             </Col>
             <Col flex={2}>
               <select
@@ -88,9 +146,7 @@ const Resident_dashboard = () => {
                 <option value="v3">Variable 3</option>
               </select>
             </Col>
-            <Col flex={2}>
-              <Button onClick={handleClick}>Display Graph</Button>
-            </Col>
+            <Col flex={2}></Col>
           </Row>
         </Content>
       </Layout>
@@ -99,19 +155,40 @@ const Resident_dashboard = () => {
       {/* <label>pick a display</label> */}
       {/* </div> */}
 
-      {temp === "true" && image === "Bar" && (
+      {image === "BarImage" && (
         <div className="content">
-          <img className="graph" src={Bar} alt="picture" />
+          <BarChart
+            width={1000}
+            height={600}
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+            <Bar dataKey="amt" stackId="a" fill="#82ca9d" />
+            <Bar dataKey="uv" fill="#ffc658" />
+          </BarChart>
+
+          {/* <img className="graph" src={BarImage} alt="picture" /> */}
         </div>
       )}
 
-      {temp === "true" && image === "Pie" && (
+      {image === "Pie" && (
         <div className="content">
           <img className="graph" src={Pie} alt="picture" />
         </div>
       )}
 
-      {temp === "true" && image === "Scatter" && (
+      {image === "Scatter" && (
         <div className="content">
           <img className="graph" src={Scatter} alt="picture" />
         </div>
