7e834650e1e398b1013e5762f941913454bc51f7
winniewjeng <winniewjeng@gmail.com>
2023-03-12 00:39:23 -0500

    clean up/delete example square.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const square = require("./square");
 const database = require("./database");
 const app = express();
 const port = 8000;
@@ -8,17 +7,6 @@ app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
 
-// app.get("/", function (req, res) {
-//   res.send(`The area of a square with a width of 4 is ${square.area(4)}~`);
-// });
-
 app.get("/user", function (req, res) {
   res.send(database("apple"));
 });
-
-// // Here we require() the name of the file without the (optional) .js file extension
-// console.log(`something something`);
-// setTimeout(function () {
-//   console.log("First");
-// }, 3000);
-// console.log("Second");
