dc859decbe31876cd5f1f9893769ab02f863ed8c
Brandon Ding <dexon888@gmail.com>
2023-03-12 05:08:30 -0400

    can add csv content to dashboard

--- src/components/dashboard/resident_dashboard.js
+++ src/components/dashboard/resident_dashboard.js
@@ -145,6 +145,48 @@ const Resident_dashboard = () => {
     }
   };
 
+  const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
+
+  const fileReader = new FileReader();
+
+  const handleOnChange = (e) => {
+      setFile(e.target.files[0]);
+  };
+
+  const csvFileToArray = string => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+
+    const array = csvRows.map(i => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray(array);
+    const mappedArray = array.map(d => Array.from(Object.values(d)))
+    console.log(mappedArray[0][0])
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      fileReader.onload = function (event) {
+        const text = event.target.result;
+        csvFileToArray(text)
+      };
+
+      fileReader.readAsText(file);
+    }
+  };
+
+  const headerKeys = Object.keys(Object.assign({}, ...array));
+
   return (
     <>
       <Layout>
@@ -181,10 +223,45 @@ const Resident_dashboard = () => {
           </Row>
         </Content>
       </Layout>
-      <form>
-          <input type={"file"} accept={".csv"} />
-          <button>Generate Data</button>
-      </form>
+      <div style={{ textAlign: "center" }}>
+            <form>
+                <input
+                    type={"file"}
+                    id={"csvFileInput"}
+                    accept={".csv"}
+                    onChange={handleOnChange}
+                />
+                <button
+                    onClick={(e) => {
+                        handleOnSubmit(e);
+                    }}
+                >
+                    Generate Data
+                </button>
+            </form>
+        </div>
+
+      <br />
+
+      <table>
+        <thead>
+          <tr key={"header"}>
+            {headerKeys.map((key) => (
+              <th>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {array.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((val) => (
+                <td>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
       {image === "BarImage" && (
         <div className="content">
