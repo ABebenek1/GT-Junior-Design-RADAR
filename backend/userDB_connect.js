2786d070255323a12b9ccd40b6b628298fb83529
Alex Bebenek <79236542+ABebenek1@users.noreply.github.com>
2023-03-12 19:23:01 -0400

    Added Create User function
    The function createUser will make a new user in the MongoDB database. It works for both admins and users, but you must specify which and you must use the exact user type string ("Resident" or "Administrator") in order for the function to identify the correct collection.

--- backend/userDB_connect.js
+++ backend/userDB_connect.js
@@ -5,6 +5,19 @@ async function main() {
     const client = new MongoClient(uri);
     try {
         await client.connect();
+
+        /* This user has already been created, but I'm leaving them as an example
+        await createUser(client, 
+             {
+                 email: "test.admin3@emory.edu",
+                 Username: "TestAdmin3",
+                 Password: "[encrypted password here]",
+                 FirstName: "Test",
+                 LastName: "Admin3"
+             }, "Administrator");
+        */
+        
+
     } catch (e) {
         console.error(e);
     } finally {
@@ -13,3 +26,8 @@ async function main() {
 }
 
 main().catch(console.error)
+
+async function createUser(client, newUser, userType){
+    const result = await client.db("RADAR_Project").collection(userType + "s").insertOne(newUser);
+    console.log(`New listing created with the following id: ${result.insertedID}`);
+}
