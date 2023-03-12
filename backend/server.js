5f24177e257380cfa2846e4545f04673e6a0e4cf
winniewjeng <winniewjeng@gmail.com>
2023-03-12 01:47:50 -0500

    installed cors to resolve network issue

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const database = require("./database");
 const app = express();
 const port = 8000;
+
+app.use(cors());
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
