6ba2bc27539a7c7cb24554c07df87a7ae6fa222e
winniewjeng <winniewjeng@gmail.com>
2023-03-31 12:05:07 -0400

    cond rendering invalid cred and cleanup

--- src/components/login.js
+++ src/components/login.js
@@ -21,50 +21,45 @@ const contentStyle = {
   minHeight: 120,
   color: "black",
   backgroundColor: "#108ee9",
-
 };
 
 const parentDiv = {
-  width:"600px",
-  height:"500px",
-  backgroundColor:"lightgrey",
-  display:"flex",
-  alignItems:"center",
-  justifyContent:"center",
-  borderRadius:"50px",
-}
+  width: "600px",
+  height: "500px",
+  backgroundColor: "lightgrey",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "50px",
+};
 
 const divStyle = {
-  width:"95%",
-  height:"400px",
-  // backgroundColor:"red"
-
-}
+  width: "95%",
+  height: "400px",
+};
 
 const titleStyle = {
-  width:"100%",
-  height:"100px",
-  display:"flex",
-  justifyContent:"center",
-  alignItems:"center"
-}
+  width: "100%",
+  height: "100px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 const containerStyle = {
-  width:"100%",
-  display:"flex",
-  alignItems:"center",
-  justifyContent:"center",
-
-} 
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
 const checkboxStyle = {
-  width:"200px",
-}
+  width: "200px",
+};
 
 const pad_down = {
-  paddingBottom:"10px"
-  
-}
+  paddingBottom: "10px",
+};
 
 export default function Login() {
   const [signinError, setSignInError] = useState(null);
@@ -107,123 +102,91 @@ export default function Login() {
       <Layout>
         <Content style={contentStyle}>
           <div style={parentDiv}>
-
-          <div style ={divStyle}>
-
-          <div style={titleStyle}> <Title>Welcome to RADAR</Title> </div>
-
-          <Row>
-            <Col flex={2}></Col>
-            <Col flex={3}>
-              <Form
-                name="basic"
-                // labelCol={{
-                //   span: 8,
-                // }}
-                // wrapperCol={{
-                //   span: 16,
-                // }}
-                // style={{
-                //   maxWidth: 600,
-                // }}
-                initialValues={{
-                  remember: true,
-                }}
-                onFinish={onFinish}
-                // onFinishFailed={onFinishFailed}
-                autoComplete="off"
-              >
-                <div>
-                <Form.Item
-                  label="Username"
-                  name="username"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your username!",
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-                </div>
-
-
-                <Form.Item
-                  label="Password"
-                  name="password"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your password!",
-                    },
-                  ]}
-                >
-                  <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                  name="remember"
-                  valuePropName="checked"
-                  style={containerStyle}
-                  // wrapperCol={{
-                  //   offset: 8,
-                  //   span: 16,
-                  // }}
-                >
-                <div style={checkboxStyle}><Checkbox>Remember me</Checkbox></div>
-                
-                </Form.Item>
-
-                <Form.Item
-                  // wrapperCol={{
-                  //   offset: 8,
-                  //   span: 16,
-                  // }}
-
-                  style={containerStyle}
-                >
-                  <Button type="primary" htmlType="submit">
-                    Log in
-                  </Button>
-                </Form.Item>
-
-              </Form>
-            </Col>
-            <Col flex={2}></Col>
-          </Row>
-
-          <Row style={pad_down}>
-            <Col span={24}>
-              New User?
-              <Link to="/sign-up" style={{ color: "yellow" }}>
+            <div style={divStyle}>
+              <div style={titleStyle}>
                 {" "}
-                Register{" "}
-              </Link>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={24}>
-              <Link to="/sign-up" style={{ color: "yellow" }}>
-                {" "}
-                Forget Password
-              </Link>
-            </Col>
-          </Row>
-          {/* <Row>
-            <Col span={24}>
-              <img
-                src={EmoryLogo}
-                alt="logo"
-                className="logo"
-                style={{ height: "260px" }}
-              />
-            </Col>
-          </Row> */}
+                <Title>Welcome to RADAR</Title>{" "}
+              </div>
+              {/* conditional rendering / null coalescing: AND short circuiting*/}
+              {signinError && <Row>{signinError}</Row>}
+              <Row>
+                <Col flex={2}></Col>
+                <Col flex={3}>
+                  <Form
+                    name="basic"
+                    initialValues={{
+                      remember: true,
+                    }}
+                    onFinish={onFinish}
+                    autoComplete="off"
+                  >
+                    <div>
+                      <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[
+                          {
+                            required: true,
+                            message: "Please input your username!",
+                          },
+                        ]}
+                      >
+                        <Input />
+                      </Form.Item>
+                    </div>
 
-          </div>
-          </div>
+                    <Form.Item
+                      label="Password"
+                      name="password"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your password!",
+                        },
+                      ]}
+                    >
+                      <Input.Password />
+                    </Form.Item>
 
+                    <Form.Item
+                      name="remember"
+                      valuePropName="checked"
+                      style={containerStyle}
+                    >
+                      <div style={checkboxStyle}>
+                        <Checkbox>Remember me</Checkbox>
+                      </div>
+                    </Form.Item>
+
+                    <Form.Item style={containerStyle}>
+                      <Button type="primary" htmlType="submit">
+                        Log in
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                </Col>
+                <Col flex={2}></Col>
+              </Row>
+
+              <Row style={pad_down}>
+                <Col span={24}>
+                  New User?
+                  <Link to="/sign-up" style={{ color: "yellow" }}>
+                    {" "}
+                    Register{" "}
+                  </Link>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={24}>
+                  <Link to="/sign-up" style={{ color: "yellow" }}>
+                    {" "}
+                    Forget Password
+                  </Link>
+                </Col>
+              </Row>
+            </div>
+          </div>
         </Content>
       </Layout>
     </div>
