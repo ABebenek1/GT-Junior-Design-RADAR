d1c41a52b3ef891e21631cd7d6077f8b3171dd07
Wei <wjeng7@gatech.edu>
2023-04-02 21:24:12 -0400

    cleanup

--- src/components/dashboard/admin_dashboard.js
+++ src/components/dashboard/admin_dashboard.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "./admin_dashboard.css";
 
 // ANTD UI
-import { Col, Row } from "antd";
-import { Layout } from "antd";
+import { Layout, Col, Row } from "antd";
+import { Popover } from "antd";
 import { Typography } from "antd";
 
 const { Title } = Typography;
@@ -22,37 +22,186 @@ const headerStyle = {
 
 // dummy data
 const dummyDataList = [
-  {id:"1",    firstname:"Reginald",   lastname:"Aguilar",     email:"dee_gulgowski87@yahoo.com",        year:"1"},
-  {id:"2",    firstname:"Dolores",    lastname:"Howard",      email:"saige_graham@hotmail.com",         year:"2"},
-  {id:"3",    firstname:"Kristi",     lastname:"Drake",       email:"nona82@gmail.com",                 year:"3"},
-  {id:"4",    firstname:"Yolanda",    lastname:"Tucker",      email:"angelina70@gmail.com",             year:"2"},
-  {id:"5",    firstname:"Marcus",     lastname:"Cunningham",  email:"kim57@hotmail.com",                year:"2"},
-  {id:"6",    firstname:"Benjamin",   lastname:"Ramirez",     email:"frances1@hotmail.com",             year:"2"},
-  {id:"7",    firstname:"Tara",       lastname:"Oliver",      email:"neva_parker21@yahoo.com",          year:"1"},
-  {id:"8",    firstname:"Melinda",    lastname:"Newton",      email:"velva_homenick65@hotmail.com",     year:"3"},
-  {id:"9",    firstname:"Blake",      lastname:"Myers",       email:"rebekah56@yahoo.com",              year:"3"},
-  {id:"10",   firstname:"Tabitha",    lastname:"Carlson",     email:"vicenta12@yahoo.com",              year:"3"},
-  {id:"11",   firstname:"Diane",      lastname:"Mann",        email:"rod_kutch46@yahoo.com",            year:"2"},
-  {id:"12",   firstname:"Tommie",     lastname:"Castro",      email:"casandra_boehm@gmail.com",         year:"3"},
-  {id:"13",   firstname:"Mike",       lastname:"Rhodes",      email:"trinity.gusikowski13@yahoo.com",   year:"2"},
-  {id:"14",   firstname:"Brittany",   lastname:"Harris",      email:"jonatan.schaefer43@gmail.com",     year:"2"},
-  {id:"15",   firstname:"Jackie",     lastname:"Barker",      email:"elijah_blanda@yahoo.com",          year:"1"},
-  {id:"16",   firstname:"Jo",         lastname:"Clayton",     email:"demarcus.reinger@yahoo.com",       year:"1"},
-  {id:"17",   firstname:"Annette",    lastname:"Daniel",      email:"anjali_jacobson57@hotmail.com",    year:"1"},
-  {id:"18",   firstname:"Kathleen",   lastname:"Sparks",      email:"leonora.lynch55@gmail.com",        year:"2"},
-  {id:"19",   firstname:"Damon",      lastname:"Moreno",      email:"trycia.williamson@hotmail.com",    year:"3"},
-  {id:"20",   firstname:"Irving",     lastname:"Dixon",       email:"bailee_macejkovic@gmail.com",      year:"1"},
-  {id:"21",   firstname:"Andrew",     lastname:"Doyle",       email:"neal85@yahoo.com",                 year:"3"},
-  {id:"22",   firstname:"Audrey",     lastname:"Curtis",      email:"isom_bailey@gmail.com",            year:"2"},
-  {id:"23",   firstname:"Randall",    lastname:"James",       email:"floyd_daniel@gmail.com",           year:"1"},
-  {id:"24",   firstname:"Terrence",   lastname:"Hampton",     email:"nasir31@gmail.com",                year:"2"},
-  {id:"25",   firstname:"Kelli",      lastname:"Manning",     email:"willy.hansen@hotmail.com",         year:"3"},
-]
+  {
+    id: "1",
+    firstname: "Reginald",
+    lastname: "Aguilar",
+    email: "dee_gulgowski87@yahoo.com",
+    year: "1",
+  },
+  {
+    id: "2",
+    firstname: "Dolores",
+    lastname: "Howard",
+    email: "saige_graham@hotmail.com",
+    year: "2",
+  },
+  {
+    id: "3",
+    firstname: "Kristi",
+    lastname: "Drake",
+    email: "nona82@gmail.com",
+    year: "3",
+  },
+  {
+    id: "4",
+    firstname: "Yolanda",
+    lastname: "Tucker",
+    email: "angelina70@gmail.com",
+    year: "2",
+  },
+  {
+    id: "5",
+    firstname: "Marcus",
+    lastname: "Cunningham",
+    email: "kim57@hotmail.com",
+    year: "2",
+  },
+  {
+    id: "6",
+    firstname: "Benjamin",
+    lastname: "Ramirez",
+    email: "frances1@hotmail.com",
+    year: "2",
+  },
+  {
+    id: "7",
+    firstname: "Tara",
+    lastname: "Oliver",
+    email: "neva_parker21@yahoo.com",
+    year: "1",
+  },
+  {
+    id: "8",
+    firstname: "Melinda",
+    lastname: "Newton",
+    email: "velva_homenick65@hotmail.com",
+    year: "3",
+  },
+  {
+    id: "9",
+    firstname: "Blake",
+    lastname: "Myers",
+    email: "rebekah56@yahoo.com",
+    year: "3",
+  },
+  {
+    id: "10",
+    firstname: "Tabitha",
+    lastname: "Carlson",
+    email: "vicenta12@yahoo.com",
+    year: "3",
+  },
+  {
+    id: "11",
+    firstname: "Diane",
+    lastname: "Mann",
+    email: "rod_kutch46@yahoo.com",
+    year: "2",
+  },
+  {
+    id: "12",
+    firstname: "Tommie",
+    lastname: "Castro",
+    email: "casandra_boehm@gmail.com",
+    year: "3",
+  },
+  {
+    id: "13",
+    firstname: "Mike",
+    lastname: "Rhodes",
+    email: "trinity.gusikowski13@yahoo.com",
+    year: "2",
+  },
+  {
+    id: "14",
+    firstname: "Brittany",
+    lastname: "Harris",
+    email: "jonatan.schaefer43@gmail.com",
+    year: "2",
+  },
+  {
+    id: "15",
+    firstname: "Jackie",
+    lastname: "Barker",
+    email: "elijah_blanda@yahoo.com",
+    year: "1",
+  },
+  {
+    id: "16",
+    firstname: "Jo",
+    lastname: "Clayton",
+    email: "demarcus.reinger@yahoo.com",
+    year: "1",
+  },
+  {
+    id: "17",
+    firstname: "Annette",
+    lastname: "Daniel",
+    email: "anjali_jacobson57@hotmail.com",
+    year: "1",
+  },
+  {
+    id: "18",
+    firstname: "Kathleen",
+    lastname: "Sparks",
+    email: "leonora.lynch55@gmail.com",
+    year: "2",
+  },
+  {
+    id: "19",
+    firstname: "Damon",
+    lastname: "Moreno",
+    email: "trycia.williamson@hotmail.com",
+    year: "3",
+  },
+  {
+    id: "20",
+    firstname: "Irving",
+    lastname: "Dixon",
+    email: "bailee_macejkovic@gmail.com",
+    year: "1",
+  },
+  {
+    id: "21",
+    firstname: "Andrew",
+    lastname: "Doyle",
+    email: "neal85@yahoo.com",
+    year: "3",
+  },
+  {
+    id: "22",
+    firstname: "Audrey",
+    lastname: "Curtis",
+    email: "isom_bailey@gmail.com",
+    year: "2",
+  },
+  {
+    id: "23",
+    firstname: "Randall",
+    lastname: "James",
+    email: "floyd_daniel@gmail.com",
+    year: "1",
+  },
+  {
+    id: "24",
+    firstname: "Terrence",
+    lastname: "Hampton",
+    email: "nasir31@gmail.com",
+    year: "2",
+  },
+  {
+    id: "25",
+    firstname: "Kelli",
+    lastname: "Manning",
+    email: "willy.hansen@hotmail.com",
+    year: "3",
+  },
+];
 
 const Admin_dashboard = () => {
-
   const UserElement = (props) => {
-    const {id, firstname, lastname, email, year} = props.user
+    const { id, firstname, lastname, email, year } = props.user;
     return (
       <tr id={id}>
         <td>{id}</td>
@@ -68,19 +217,17 @@ const Admin_dashboard = () => {
           </select>
           {/* <button onClick={testing}>Confirm</button> */}
         </td>
-
       </tr>
-    )
-  }
+    );
+  };
 
   function ddaction(e) {
-    let row = e.target.parentNode.parentNode
+    let row = e.target.parentNode.parentNode;
     let rowdata = document.getElementById(row.id).querySelectorAll("td");
     let name = rowdata[1].innerHTML;
 
     let action = e.target.value;
     if (action === "delete") {
-
       let text = "Please confirm that you want delete user: " + name;
 
       if (window.confirm(text) === true) {
@@ -88,11 +235,9 @@ const Admin_dashboard = () => {
 
         for (let i = 0; i < dummyDataList.length; i++) {
           if (dummyDataList[i].id === row.id) {
-            dummyDataList.splice(i, 1)
+            dummyDataList.splice(i, 1);
           }
-
         }
-
       }
     }
 
@@ -122,21 +267,19 @@ const Admin_dashboard = () => {
         </Header>
       </Layout>
 
-      <table style={{width:"100%", borderCollapse:"collapse"}}>
+      <table style={{ width: "100%", borderCollapse: "collapse" }}>
         <tr>
-          <th style={{width:"10%"}}>ID</th>
+          <th style={{ width: "10%" }}>ID</th>
           <th>Last, First</th>
           <th>Email</th>
-          <th style={{width:"10%"}}>Year</th>
-          <th style={{width:"25%"}}>Action</th>
+          <th style={{ width: "10%" }}>Year</th>
+          <th style={{ width: "25%" }}>Action</th>
         </tr>
 
         {dummyDataList.map((user) => {
-          return <UserElement user={user}></UserElement>
+          return <UserElement user={user}></UserElement>;
         })}
-
       </table>
-
     </>
   );
 };
